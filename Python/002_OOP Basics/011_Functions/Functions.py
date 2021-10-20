6422f8e60d10396ad4390b9b8b6e1c1612e9bb6e
JCOCA-Tech <64308718+JCOCA-Tech@users.noreply.github.com>
2021-10-20 17:34:54 +0200

    finished Functions.py

---
+++ Python/002_OOP Basics/011_Functions/Functions.py
@@ -0,0 +1,6 @@
+def myFunction(name, age): # This is our own function that greets the user
+    print("Hello ", name, " you are ", age)
+
+myFunction("John", 50) # This calls our function with the name and age parameters
+myFunction("Bill", 37)
+myFunction("Mike", 30)
