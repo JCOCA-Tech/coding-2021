e81a770a10f8a11309a824d893c90aae70e85eb6
JCOCA-Tech <64308718+JCOCA-Tech@users.noreply.github.com>
2021-10-22 21:27:25 +0200

    python version of for loops

--- Python/002_OOP Basics/019_For Loops/ForLoops.py
+++ Python/002_OOP Basics/019_For Loops/ForLoops.py
@@ -1,30 +1,16 @@
-index = 1
+index = 0
 
+"""
 while(index <= 5):
     print(index)
     index+=1
+"""
 
 """
-int index = 1;
-
-/*
-while(index <= 5)
-{
-    cout << index << endl;
-    index++;
-}
-*/
-
-/*
-for(int index = 1; index <= 5; index++) // Does the same as the above but is way more compact
-{
-    cout << index << endl;
-}
-*/
-
-int nums[] = {1, 2, 5, 7, 3};
-for(int index = 0; index <= 4; index++) // For loops can be used to loop trough the elements of an array
-{
-    cout << nums[index] << endl;
-}
+for index in range(5): # Does the same as the above but is way more compact
+    print(index)
 """
+
+numbers = {1, 2, 5, 7, 3}
+for number in numbers: # For loops can be used to loop trough the elements of an array
+    print(number)
