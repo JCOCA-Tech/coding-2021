b0407c63889882b45e26ff96c10904babf76c78c
JCOCA-Tech <64308718+JCOCA-Tech@users.noreply.github.com>
2021-10-23 15:13:58 +0200

    python versions of freecodecamp programs

---
+++ Python/002_OOP Basics/022_Comments/Comments.py
@@ -0,0 +1,15 @@
+# Comment
+
+"""
+Using an opening and closing tag for multiline comments
+"""
+
+"""
+Comments are often written for users, yourself or other developers
+"""
+
+# This prints out "Comments are fun" on the console
+print("Comments are fun")
+
+#print("Comments are not fun")
+# The line above is commented out. It will not affect the program any longer
---
+++ Python/002_OOP Basics/023_Pointers/Pointers.py
@@ -0,0 +1,9 @@
+"""
+
+Pointers do not exist in Python.
+If you want to know why i recommend reading this article:
+
+    https://realpython.com/pointers-in-python/
+
+
+"""
