161513ce9956dfa77028faa207282d08c2e6438e
JCOCA-Tech <64308718+JCOCA-Tech@users.noreply.github.com>
2021-10-31 15:52:13 +0100

    updated added cmake tutorial files

---
+++ CMake/007_Adding a Library/build/CMakeFiles/say_hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/say_hello.dir/hello.cpp.o"
+  "CMakeFiles/say_hello.dir/hello.cpp.o.d"
+  "libsay_hello.a"
+  "libsay_hello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/say_hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
